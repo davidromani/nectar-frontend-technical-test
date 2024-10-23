4955b3362b412defe662ca4230bc3f732b9019ac
David Romaní <david@flux.cat>
2024-10-23 18:15:44 +0200

    remove unnecessary tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,24 +20,9 @@ import { UserData } from './providers/user-data';
 export class AppComponent implements OnInit {
   appPages = [
     {
-      title: 'Schedule',
+      title: 'Task',
       url: '/app/tabs/schedule',
       icon: 'calendar'
-    },
-    {
-      title: 'Speakers',
-      url: '/app/tabs/speakers',
-      icon: 'people'
-    },
-    {
-      title: 'Map',
-      url: '/app/tabs/map',
-      icon: 'map'
-    },
-    {
-      title: 'About',
-      url: '/app/tabs/about',
-      icon: 'information-circle'
     }
   ];
   loggedIn = false;
