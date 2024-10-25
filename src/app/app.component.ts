ada74508a6b3b60dc267309e333427f7f9fc22d1
David Romaní <david@flux.cat>
2024-10-25 15:51:31 +0200

    remove tasks side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { SwUpdate } from '@angular/service-worker';
 
-import { MenuController, Platform, ToastController } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 
 import { StatusBar } from '@capacitor/status-bar';
 import { SplashScreen } from '@capacitor/splash-screen';
@@ -19,17 +19,16 @@ import { UserData } from './providers/user-data';
 })
 export class AppComponent implements OnInit {
   appPages = [
-    {
+    /*{
       title: 'Task',
       url: '/tasks/tasks',
       icon: 'calendar'
-    }
+    }*/
   ];
   loggedIn = false;
   dark = false;
 
   constructor(
-    private menu: MenuController,
     private platform: Platform,
     private router: Router,
     private storage: Storage,
